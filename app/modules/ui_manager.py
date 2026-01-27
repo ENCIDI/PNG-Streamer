0c972caa5f574321c273284d9d3a54568a2143c0
ENCIDI <hellhuger@gmail.com>
2026-01-27 15:13:24 +0100

    Add speech recognition fallback for microphone list

--- app/modules/ui_manager.py
+++ app/modules/ui_manager.py
@@ -1,4 +1,5 @@
 from PyQt6 import uic
+from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QDialog
 import sys
@@ -23,8 +24,22 @@ class PNGStreamerApp(QDialog):
         self.microChoose.currentIndexChanged.connect(self.micro_changed)
         self.noiseCheckbox.stateChanged.connect(self.noise_toggled)
 
+        self.settings = sm.load_settings()
+        self._populate_microphones()
+        self._apply_settings_to_ui()
+        self._start_volume_updates()
+
     def apply_settings(self):
-        pass
+        selected_device = self.microChoose.currentData()
+        selected_name = self.microChoose.currentText() or "Default"
+        sm.update_settings(
+            {
+                "microphone": selected_name,
+                "unnoised": self.noiseCheckbox.isChecked(),
+            }
+        )
+        am.set_noise_suppression(self.noiseCheckbox.isChecked())
+        am.start_monitor(selected_device)
 
     def edit_profile(self):
         pass
@@ -43,10 +58,44 @@ class PNGStreamerApp(QDialog):
         pass
 
     def micro_changed(self, index):
-        pass
+        selected_device = self.microChoose.itemData(index)
+        am.start_monitor(selected_device)
 
     def noise_toggled(self, state):
-        pass
+        enabled = self.noiseCheckbox.isChecked()
+        am.set_noise_suppression(enabled)
+        sm.update_settings({"unnoised": enabled})
+
+    def _populate_microphones(self):
+        self.microChoose.clear()
+        devices = am.list_input_devices()
+        for device in devices:
+            self.microChoose.addItem(device.name, device.device_id)
+
+    def _apply_settings_to_ui(self):
+        settings = self.settings.get("settings", {})
+        microphone = settings.get("microphone", "Default")
+        if isinstance(microphone, int):
+            target_index = self.microChoose.findData(microphone)
+        else:
+            target_index = self.microChoose.findText(str(microphone))
+        if target_index == -1:
+            target_index = 0
+        self.microChoose.setCurrentIndex(target_index)
+        self.noiseCheckbox.setChecked(bool(settings.get("unnoised", False)))
+        selected_device = self.microChoose.currentData()
+        am.set_noise_suppression(self.noiseCheckbox.isChecked())
+        am.start_monitor(selected_device)
+
+    def _start_volume_updates(self):
+        self.volume_timer = QTimer(self)
+        self.volume_timer.setInterval(200)
+        self.volume_timer.timeout.connect(self._update_volume_label)
+        self.volume_timer.start()
+
+    def _update_volume_label(self):
+        volume = am.get_current_volume()
+        self.volumeLabel.setText(str(int(volume)))
 
 class ProfilesCustomizationScreen(QDialog):
     def __init__(self):
